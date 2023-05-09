ffc77bb646935ddf5b10656a73337924a84b114c
Danh Le <danh.le@successsoftware.global>
2023-05-09 11:44:40 +0700

    .x9

--- src/template/HomeTemplate.tsx
+++ src/template/HomeTemplate.tsx
@@ -54,8 +54,14 @@ export default function HomeTemplate() {
       )} */}
 
       <div onClick={hide}>
-        <button style={{position:'fixed', bottom:'20px', right:'20px', backgroundColor:'#007bff', color:'#fff', border:'none', padding:'10px 20px', fontSize:'16px', cursor:'pointer', zIndex:'999'}} onClick={promptToInstall}>Add to homescreen</button>
+        <button onClick={hide}>Close</button>
+        Hello! Wanna add to homescreen?
+        <button onClick={promptToInstall}>Add to homescreen</button>
       </div>
+
+      {/* <div onClick={hide}>
+        <button style={{ position: 'fixed', bottom: '20px', right: '20px', backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '10px 20px', fontSize: '16px', cursor: 'pointer', zIndex: '999' }} onClick={promptToInstall}>Add to homescreen</button>
+      </div> */}
 
       {/* Header */}
       <Header />
