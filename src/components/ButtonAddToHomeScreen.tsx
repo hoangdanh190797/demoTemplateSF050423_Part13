58db35fb077236bf3314228bad5c167166d50d71
Danh Le <danh.le@successsoftware.global>
2023-05-09 10:19:02 +0700

    ...

--- src/components/ButtonAddToHomeScreen.tsx
+++ src/components/ButtonAddToHomeScreen.tsx
@@ -6,7 +6,6 @@ const AddToHomeScreenButton= ({promptEvent}:any) => {
       promptEvent.prompt();
     }
   };
-
   return (
     <button style={{position:'fixed', bottom:'20px', right:'20px', backgroundColor:'#007bff', color:'#fff', border:'none', padding:'10px 20px', fontSize:'16px', cursor:'pointer', zIndex:'999'}} onClick={handleClick}>
       Add to Home Screen
--- src/template/HomeTemplate.tsx
+++ src/template/HomeTemplate.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
@@ -9,22 +8,18 @@ import ButtonAddToHomeScreen from '../components/ButtonAddToHomeScreen'
 export default function HomeTemplate() {
   const [promptEvent, setPromptEvent] = useState<any>(null);
   const IS_BROWSER = typeof window !== 'undefined';
-
-
   useEffect(() => {
     if (IS_BROWSER) {
       const handler = (event: any) => {
         event.preventDefault();
         setPromptEvent(event);
-        console.log(event)
       };
       window.addEventListener("beforeinstallprompt", handler);
       return () => {
         window.removeEventListener("beforeinstallprompt", handler);
       }
     };
-
-  }, [promptEvent]);
+  }, [setPromptEvent, promptEvent]);
 
   return (
     <>
